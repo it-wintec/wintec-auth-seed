82300f1ba4137e6117e8fffe1e2e7170d062b0da
Jack Yang <daoyan04@student.wintec.ac.nz>
2018-07-19 01:11:18 +0000

    third commit

--- hello-world.php
+++ hello-world.php
@@ -4,7 +4,7 @@
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo 'Hello world from Jack!';
+echo 'Hello world from Jack & Junpeng!';
 
 ?>
 </body>
