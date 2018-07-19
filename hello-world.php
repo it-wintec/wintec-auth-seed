19c2050fc0d50ea0a4885359716cf9beff6197e9
Jack Yang <daoyan04@student.wintec.ac.nz>
2018-07-19 01:07:57 +0000

    second commit

--- hello-world.php
+++ hello-world.php
@@ -4,7 +4,7 @@
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo 'Hello world from Cloud9!';
+echo 'Hello world from Jack!';
 
 ?>
 </body>
