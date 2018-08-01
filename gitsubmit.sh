a3b495651f6cc9f65df404243e590e895321f996
Jack Yang <daoyan04@student.wintec.ac.nz>
2018-08-01 04:17:58 +0000

    No specified information

--- gitsubmit.sh
+++ gitsubmit.sh
@@ -2,8 +2,11 @@
 
 echo "输入更新日志[ENTER]:"
 read text
+if [[ -z "$text" ]]; then
+    text="No specified information"
+fi
 
 git status
 git add -A .
-git commit -m $text
+git commit -m "$text"
 git push origin master
